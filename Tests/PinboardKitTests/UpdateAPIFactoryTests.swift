492656484018d44fb52eb467c5f0082366864398
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2022-01-09 09:36:16 +0100

    Add UpdateAPIFactoryTests

---
+++ Tests/PinboardKitTests/UpdateAPIFactoryTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+import PinboardKit
+
+final class UpdateAPIFactoryTests: XCTestCase {
+
+    func testMakeCheckFollowingRequest() throws {
+        let request = UpdateAPIFactory.makeUpdateRequest()
+
+        XCTAssertEqual(
+            request.path, "/v1/posts/update"
+        )
+
+        XCTAssertEqual(
+            request.method,
+            .get
+        )
+
+        XCTAssertNil(request.body)
+        XCTAssertNil(request.decoder)
+        XCTAssertNil(request.encoder)
+    }
+}
